47c8c279eb5216af31c0cfe443f8f9cb6c9d61a9
Yuki Iwai <waiwai.cps@gmail.com>
2024-01-16 16:44:20 +0900

    依存を修正

--- src/webhook/line/line.module.ts
+++ src/webhook/line/line.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LineController } from './line.controller';
 import { SpellService } from '../spell.service';
+import { AiService } from '../ai.service';
 
 @Module({
   imports: [],
   controllers: [LineController],
-  providers: [SpellService],
+  providers: [SpellService, AiService],
 })
 export class LineModule {}
